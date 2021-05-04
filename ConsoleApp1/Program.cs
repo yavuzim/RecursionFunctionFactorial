dff7939d14092e85042656617cf9d6b59b537be7
kemal <yavuz.imer133@gmail.com>
2021-05-04 18:30:03 +0300

    RecursionFunctionFactorial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int sayac = 0, sonuc = 1;
+        static int Faktoriyel(int sayi)
+        {
+            if (sayi == 0 || sayi == 1)
+                return 1;
+            if (1 < sayi)
+            {
+                sayac++;
+                sayi--;
+                sonuc *= sayi;
+                Faktoriyel(sayi);
+            }
+            return sonuc;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("  Sayı : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int faktoriyel = Faktoriyel(sayi + 1);
+            Console.WriteLine("  " + sayi + "!" + " : " + faktoriyel);
+            Console.Read();
+
+        }
+    }
+}
