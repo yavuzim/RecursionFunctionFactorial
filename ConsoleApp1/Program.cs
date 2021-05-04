b51dd336aa66546c5ae74f8f00d4ee912a997f15
kemal <yavuz.imer133@gmail.com>
2021-05-04 18:58:07 +0300

    RecursionFunctionFactorial

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,17 +4,16 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static int sayac = 0, sonuc = 1;
+        static int  sonuc = 1;
         static int Faktoriyel(int sayi)
         {
             if (sayi == 0 || sayi == 1)
                 return 1;
             if (1 < sayi)
             {
-                sayac++;
                 sayi--;
                 sonuc *= sayi;
-                Faktoriyel(sayi);
+              Faktoriyel(sayi);
             }
             return sonuc;
         }
@@ -25,7 +24,6 @@ namespace ConsoleApp1
             int faktoriyel = Faktoriyel(sayi + 1);
             Console.WriteLine("  " + sayi + "!" + " : " + faktoriyel);
             Console.Read();
-
         }
     }
 }
